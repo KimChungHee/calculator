b95a636d45b3d1c9f66704f0d84dd210e219f83b
Jaemin Choi <jaemin.jay@gmail.com>
2025-04-10 17:09:21 +0900

    Add minus operation
    Add getMinus function that operates minus.
    
    Signed-off-by: Jaemin Choi <jaemin.jay@gmail.com>

--- Project8/main.cpp
+++ Project8/main.cpp
@@ -2,11 +2,21 @@
 
 class Cal {
 public:
-
+	int getMinus(a, b) {
+		return a - b;
+	}
 };
 
 TEST(t1, t2) {
 	EXPECT_EQ(1, 1);
+}
+
+
+TEST(t1, t3) {
+	int a = 5;
+	int b = 2;
+	int actual = a - b;
+	EXPECT_EQ(actual, 3);
 }
 
 int main() {
